05c64fafe3b5cd7eb81f54f4e3a7e244360e6da7
quentinlautischer <lautisch@ualberta.ca>
2015-10-02 19:30:40 -0600

    Added Junit3 test

--- app/src/androidTest/java/com/example/quentinlautischer/cmput301_assignment1/StatsModelTest.java
+++ app/src/androidTest/java/com/example/quentinlautischer/cmput301_assignment1/StatsModelTest.java
@@ -1,15 +1,32 @@
 package com.example.quentinlautischer.cmput301_assignment1;
 
+import android.app.Activity;
+import android.test.ActivityInstrumentationTestCase2;
+
 import junit.framework.TestCase;
 
 /**
  * Created by quentinlautischer on 2015-09-15.
  */
 
-public class StatsModelTest extends TestCase {
-//    public void testReactionList(){
-//        StatsModel statCtrl = new StatsModel(new MainActivity());
-//        assertTrue("Ensure StatsModel create ArrayList", statCtrl.reactionTimes.equals(new ArrayList<Integer>()));
-//    }
+public class StatsModelTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    private MainActivity root;
+
+    public StatsModelTest() {
+        super(MainActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        root = getActivity();
+    }
+
+    public void testGetInstance(){
+        StatsModel model = StatsModel.getInstance(root);
+        StatsModel modelRef = StatsModel.getInstance();
+        assertEquals(model, modelRef);
+    }
 
 }
